2aacff4372e74542145927314bd6856cc41b8bca
Thomas Sesmat <60945894+sourisimos@users.noreply.github.com>
2025-02-11 09:23:50 +0100

    Update start_api.sh

--- start_api.sh
+++ start_api.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Vérifier que le fichier "download_done.txt" existe
 echo "Attente que le téléchargement soit terminé..."
-while [ ! -f /app/data/RINE.txt ]; do
+while [ ! -f /app/data/DONE.txt ]; do
   echo "Téléchargement non terminé. Attente..."
   sleep 15
 done
+
+# Supprimer le fichier RIN.txt
+echo "Suppression du fichier DONE.txt..."
+rm /app/data/DONE.txt
+
 # Lancer l'API
 echo "Téléchargement terminé, démarrage de l'API..."
 uvicorn src.api.app:app --host 0.0.0.0 --port 8000
